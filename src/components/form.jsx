70a72f228d4c8776883b5dffa5bb50da00cb5310
EvaMalinina <userdotsenko@gmail.com>
2021-01-07 00:59:53 +0200

    +material-ui

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,38 @@
 import  React, { useState, useEffect, useRef } from 'react';
+import Button from "@material-ui/core/Button";
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, gray 30%, black 90%)',
+        border: 0,
+        borderRadius: 3,
+        color: 'white',
+        height: 55,
+        padding: '0 30px',
+        whiteSpace: 'nowrap'
+    },
+    mainInput: {
+        width: '90%'
+    },
+    label: {
+        width: '100%'
+    }
+});
+
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: '#000000' }
+    }
+});
+
 
 const Form = () => {
 
-    const [ newTodo, setNewTodo ] = useState('What\'s need to be done?');
+    const [ newTodo, setNewTodo ] = useState('');
     const [ todos, setTodos ] = useState([
         {
             text: "Learn about React",
@@ -23,7 +53,8 @@ const Form = () => {
     const inputRef = useRef();
     const noteRef = useRef();
     let UniqKey = 123;
-    const [ hint, setHint ] = useState('')
+    const [ isInputEmpty, setInputEmpty ] = useState(false)
+    const classes = useStyles();
 
 
     const handleSubmit = (e) => {
@@ -36,9 +67,10 @@ const Form = () => {
     const addTodo = text => {
         if ( text !== '') {
             const newTodos = [...todos, { text }]
+            setNewTodo('')
             setTodos(newTodos);
         } else {
-            setHint('Task can\'t be empty');
+            setInputEmpty(true);
         }
     };
 
@@ -71,12 +103,8 @@ const Form = () => {
         setNewTodo('');
     }
 
-    const clearHint = () => {
-        setHint('');
-    }
-
     const setTodo = todo => {
-        clearHint();
+        setInputEmpty(false);
         setNewTodo(todo);
     }
 
@@ -87,14 +115,36 @@ const Form = () => {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <p>{hint}</p>
-                <input
-                    value={newTodo}
-                    onChange={(e) => setTodo(e.target.value)}
-                    onFocus={clearInput}
-                    ref={inputRef}
-                />
-                <button type="submit" alt="add-note">Add</button>
+
+                <div className="form__input">
+                    <ThemeProvider theme={theme}>
+                        <FormControl   className={classes.label}>
+                            <TextField
+                                id="outlined-basic"
+                                label="What's need to be done?"
+                                value={newTodo}
+                                variant="outlined"
+                                onChange={(e) => setTodo(e.target.value)}
+                                onFocus={clearInput}
+                                ref={inputRef}
+                                className={classes.mainInput}
+                                aria-describedby="component-error-text"
+                            />
+
+                            { !isInputEmpty ?
+                                <></>
+                                :
+                                <>
+                                    <FormHelperText id="component-error-text">Task can't be empty</FormHelperText>
+                                </>
+                            }
+                        </FormControl>
+                    </ThemeProvider>
+                    {/*<div className="form__btn-wrapper">*/}
+                        <Button type="submit" alt="add-note" className={classes.root}>Add task</Button>
+                    {/*</div>*/}
+                </div>
+
                 <div className="notes">----------------------------------------
                     <ul>
                         {todos.map((todo, inx) => (
